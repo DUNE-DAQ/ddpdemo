71e1178f05ac847eaf9c197448a1edef4dbee0b9
Kurt Biery <biery@fnal.gov>
2020-10-05 23:23:58 +0200

    Initial (crude) implementation of HDF5DataStore::getAllExistingKeys()

--- include/ddpdemo/HDF5DataStore.hpp
+++ include/ddpdemo/HDF5DataStore.hpp
@@ -18,9 +18,12 @@
 #include <ers/Issue.h>
 #include <TRACE/trace.h>
 
-
 #include "highfive/H5File.hpp"
+#include <boost/lexical_cast.hpp>
 
+#include <filesystem>
+#include <memory>
+#include <regex>
 
 namespace dunedaq {
 namespace ddpdemo {
@@ -29,16 +32,17 @@ namespace ddpdemo {
 class HDF5DataStore : public DataStore {
 
 public:
-  explicit HDF5DataStore(const std::string name, const std::string& path, const std::string& fileName) : DataStore(name) {
+  explicit HDF5DataStore(const std::string name, const std::string& path, const std::string& filePattern)
+    : DataStore(name)
+    , directoryPath_(path)
+    , filePattern_(filePattern)
+  {
+    ERS_INFO("Directory path: " << path );
+    ERS_INFO("Filename pattern: " << filePattern);
 
-  ERS_INFO("Directory path: " << path );
-  ERS_INFO("Filename: " << fileName);
-
-  // Creating an empty HDF5 file
-  filePtr = new HighFive::File(path + fileName, HighFive::File::OpenOrCreate | HighFive::File::Truncate);
-  ERS_INFO("Created HDF5 file.");
- 
-
+    // Creating an empty HDF5 file
+    filePtr = new HighFive::File(path + filePattern, HighFive::File::OpenOrCreate | HighFive::File::Truncate);
+    ERS_INFO("Created HDF5 file.");
   }
 
   virtual void setup(const std::string path, const std::string fileName) {
@@ -62,13 +66,32 @@ public:
 
     // AAA: how often should we flush? After every write? 
     //filePtr->flush();
-    
   }
 
   virtual std::vector<StorageKey> getAllExistingKeys() const
   {
-    std::vector<StorageKey> emptyList;
-    return emptyList;
+    std::vector<StorageKey> keyList;
+    std::vector<std::string> fileList = getAllFiles_();
+
+    for (auto& filename : fileList)
+    {
+      std::unique_ptr<HighFive::File> localFilePtr(new HighFive::File(filename, HighFive::File::ReadOnly));
+      TLOG(TLVL_DEBUG) << get_name() << ": Opened HDF5 file " << filename;
+
+      std::vector<std::string> topLevelObjects = localFilePtr->listObjectNames();
+      for (auto& topLevelObject : topLevelObjects)
+      {
+        int eventNumber = boost::lexical_cast<int>(topLevelObject);
+
+        // to-do: determine real geoLocation and pass it into the Key
+        StorageKey thisKey(eventNumber, "none", 0);
+        keyList.push_back(thisKey);
+      }
+
+      localFilePtr.reset();  // explicit destruction
+    }
+
+    return keyList;
   }
 
 
@@ -79,6 +102,36 @@ private:
   HDF5DataStore& operator=(HDF5DataStore&&) = delete;
 
   HighFive::File* filePtr; 
+
+  std::string directoryPath_;
+  std::string filePattern_;
+
+  std::vector<std::string> getAllFiles_() const
+  {
+    // 05-Oct-2020, KAB: this method expects filename patterns of the following forms"
+    // - "<constant_string>", e.g. "demo.hdf5:
+    // - "<constant_string>%e<another_constant_string>", e.g. "demo_event%e.hdf5"
+    // - "<constant_string>%e<another_constant_string>%g<another_constant_string>", e.g. "demo_event%e_geo%g.hdf5"
+
+    // since this method is expected to find *all* files that match the expected pattern,
+    // we can simply replace any "%e" and "%g" substrings with wildcards.
+    std::string workString = filePattern_;
+    workString = std::regex_replace(workString, std::regex("\\%e"), ".*");
+    workString = std::regex_replace(workString, std::regex("\\%g"), ".*");
+    std::regex regexSearchPattern(workString);
+
+    std::vector<std::string> fileList;
+    for (const auto& entry : std::filesystem::directory_iterator(directoryPath_))
+    {
+      TLOG(TLVL_DEBUG) << "Directory element: " << entry.path().string();
+      if (std::regex_match(entry.path().filename().string(), regexSearchPattern))
+      {
+        TLOG(TLVL_DEBUG) << "Matching directory element: " << entry.path().string();
+        fileList.push_back(entry.path());
+      }
+    }
+    return fileList;
+  }
 
 };
 
